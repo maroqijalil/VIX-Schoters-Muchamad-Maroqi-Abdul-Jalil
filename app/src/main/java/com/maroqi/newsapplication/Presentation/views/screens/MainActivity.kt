beb9623a0c4935c2a192ba4f921d0081b2310415
maroqijalil <jalilpro31@gmail.com>
2022-08-30 18:45:42 +0700

    init: create android project

---
+++ app/src/main/java/com/maroqi/newsapplication/Presentation/views/screens/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maroqi.newsapplication.Presentation.views.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maroqi.newsapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
